5cf36ca0bff334ba4610dc23696885fcd4522ed3
antoniolpcan <antoniolpctwice@gmail.com>
2022-11-04 09:39:30 -0300

    add info

--- models/database.py
+++ models/database.py
@@ -1,38 +1,76 @@
 [
     {
         "nome_solo": "Latossolos"
+        # - Composto, principalmente, por material mineral. Presença de sedimentos argilosos na parta superior. 
+        # São homogêneos, apresentando baixa diferenciação entre horizontes e camadas. Sua textura é argilosa. 
+        # As cores mais comuns são: vermelho e vermelho-amarelo.
     },
     {
         "nome_solo": "Argissolos"
+        # - Formado, principalmente, por material de origem mineral. Apresenta acentuada diferenciação entre as camadas ou horizontes. 
+        # Podem ser argilosos ou arenosos. 
+        # Apresentam coloração avermelhada, amarelada ou brunada.
     },
     {
         "nome_solo": "Neossolos"
+        # - Tipos de solo que apresenta pouco desenvolvimento (azonal). 
+        # Presença de material mineral e orgânico (materiais de decomposição), além de areias quartsozas. 
+        # São solos rasos (pouca profundidade).
     },
     {
         "nome_solo": "Nitossolos"
+        # - Formado por material mineral com presença de argila. 
+        # São homogêneos, apresentando pouca diferenciação de cor com a variação da profundidade.
     },
     {
         "nome_solo": "Cambissolos"
+        # - Formado por material mineral. 
+        # Presença de argila e, em alguns casos, elementos da decomposição de riodacitos (rocha de origem vulcânica). 
+        # Possui textura média ou mais fina.
     },
     {
         "nome_solo": "Gleissolos"
+        # - Formado por material mineral. Presença de sedimentos orgânicos e argilosos do período Quaternário (Era Cenozoica). 
+        # São formados, principalmente, em condições de saturação com água. 
+        # Por isso, são encontrados, principalmente, em regiões de várzeas e planícies que sofrem inundações. 
+        # Possui, geralmente, cor cinza claro.
     },
     {
         "nome_solo": "Espodossolos"
+        # - Composto por material mineral. 
+        # É predominantemente arenoso.
     },
     {
         "nome_solo": "Vertissolos"
+        # - Solo composto por material mineral. 
+        # Solo encontrado no Brasil no bioma da Caatinga.
     },
     {
         "nome_solo": "Planossolos"
+        # - Formado principalmente por material mineral. 
+        # Apresenta baixa presença de argila em camadas superiores. 
+        # Possui permeabilidade reduzida.
     },
     {
         "nome_solo": "Luvissolos"
+        # - Presentes, principalmente, no agreste e no sertão do Nordeste. 
+        # Rico em magnésio, sódios, cálcio e argila.
     },    
     {
         "nome_solo": "Organossolos"
+        # - Solo formado basicamente por materiais orgânicos, principalmente carbono orgânico. 
+        # Possui coloração escura (cinza escuro, chumbo). 
+        # São formados em condições de saturação hídrica.
     },  
     {
         "nome_solo": "Chernossolos"
+        #  - Composto por material mineral. 
+        # Presença de sedimentos argilosos.
+    },
+    {
+        "nome_solo": "Plintossolos"
+        #  - Solo composto basicamente por material mineral. 
+        # Presença de sedimentos argilo-arenosos.
     }
+
 ]
