4c9af1960e07a7faf14727c25877ec5f7644467a
Antonio Candioto <89643628+antoniolpcan@users.noreply.github.com>
2022-11-05 12:49:04 -0300

    Add files via upload

--- models/request.py
+++ models/request.py
@@ -4,19 +4,7 @@ from typing import List
 
 class ModelForm(BaseModel):
     cor: str
-    umidade: str
+    textura: str
+    intemperizado: str
     regiao: str
-    tipo_areia: str
-    cascalho: bool
-    argila: bool
-    data_analise: datetime
-
-class Semente(BaseModel):
-    nome: str
-    preco: float
-    descricao: str
-    url: str
-
-class Resultados(BaseModel):
-    solo: str
-    sementes: List[Semente]
+    imagem: str
